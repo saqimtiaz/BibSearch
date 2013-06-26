saqimtiaz/BibSearch

commit 350880260e3b510c5106d08c710054d521f8498e

Barcode scanning: - identify if barcode isISBN or BIBSYS record id - Verify ISBN-10 and -13 - Lookup BIBSYS object id from barcode

Realfagsbibilioteket - UiO/assets/www/bibsearch.js
@@ -400,17 +400,86 @@ $.extend(BookWorms,{
 		}
 	},
 
+	validIsbn10: function(isbn) {
+		var n, i, sum = 0;
+		isbn = isbn.toString(); // make sure it's a string
+		if (isbn.length !== 10) return false;
+		for (i=0; i < 10; i++) {
+			n = (isbn[i] === 'X') ? 10 : parseInt(isbn[i]);
+			sum += n * (10-i);
+		}
+		return sum % 11 === 0;
+	},
+
+	validIsbn13: function(isbn) {
+		var check, i;
+		isbn = isbn.toString(); // make sure it's a string
+		if (isbn.length !== 13) return false;
+		check = 0;
+		for (i = 0; i < 13; i += 2) {
+			check += parseInt(isbn[i]);
+		}
+		for (i = 1; i < 12; i += 2){
+			check += 3 * parseInt(isbn[i]);
+		}
+		return check % 10 === 0;
+	},
+
+	searchByRecordId: function(knyttdokid) {
+		var url = "http://labs.biblionaut.net/services/getids.php?" + window.JSONP + "&id=" + knyttdokid;
+		$.getJSON(url, function (data) {
+			$("#searchinput1").val('bs.objektid="' + data.objektid + '"');
+			$("#search_button").click();
+		})
+		.error(function(){
+			$.mobile.hidePageLoadingMsg();
+			$('#block-ui').hide();
+		});
+	},
+
 	scanNow : function() {
 		window.plugins.barcodeScanner.scan(
 			function(result) {
-		    	//alert("We got a barcode\n" + "Result: " + result.text + "\n" + "Format: " + result.format); 
-		    	console.log(0);
-				$("#searchinput1").val(result.text);
-				$("#search_button").click();
+				//alert("We got a barcode\n" + "Result: " + result.text + "\n" + "Format: " + result.format); 
+				console.log(0);
+				var barcode = result.text,
+					format = 'unknown';
+				// Only numerals?
+
+				if (barcode.match('[0-9X]+')[0] === barcode) {
+					if (barcode.length === 10) {
+						format = 'isbn10';
+						if (!BookWorms.validIsbn10(barcode)) {
+							alert("Bad reading; not a valid ISBN-10 number. Please try again.");
+							return;
+						}
+					} else if (barcode.length === 13) {
+						format = 'isbn13';
+						if (!BookWorms.validIsbn13(barcode)) {
+							alert("Bad reading; not a valid ISBN-13 number. Please try again.");
+							return;
+						}
+					} 
+				} else {
+					if (barcode.length === 9) {
+						format = 'dokid'; // dokid eller knyttid
+					}
+				}
+				if (format === 'isbn10' || format === 'isbn13') {
+					$("#searchinput1").val('bs.isbn="' + barcode + '"');
+					$("#search_button").click();
+				} else if (format === 'dokid') {
+					BookWorms.searchByRecordId(barcode);
+					return;
+				} else {
+					alert("Unknown barcode format found.");
+					return;
+				}
 			}, 
 			function(error) {
-	    		alert("Scanning failed: " + error);
-		});
+			alert("Scanning failed: " + error);
+			}
+		);
 	},		
 
 	shareBook : function() {

commit 0c410648ffc396b6e66cf484ff55357e203d826d

fix isbn search bug introduced in 350880260e, improved query building - fix: 350880260e filled #searchinput1 instead of #searchinput2 - slightly improved isbn testing - show error message if user tries to scan HEFTID, since we can't   lookup those barcodes - use biblionaut.net instead of labs.biblionaut.net for id lookup   (faster) - don't escape search term if it specifies index (e.g. bs.isbn,   bs.dokid)

Realfagsbibilioteket - UiO/assets/www/bibsearch.js
@@ -246,20 +246,27 @@ $.extend(BookWorms,{
 		var term = parameters[0].split("=")[1];
 		var page = parameters[1].split("=")[1];
 		//XXX add timestamp
-		
-		var url;
-		
-		if (window.BookWorms.includeEbooks == "off") {
-			//url = "https://ask.bibsys.no/ask2/json/result.jsp?jsonp=?" + "&cql=" + term + '%20AND%20(bs.avdeling%20=%20"UREAL")&page=' + page;
-			url = "https://ask.bibsys.no/ask2/json/result.jsp?" + window.JSONP + "&cql=%22" + term + '%22%20AND%20(bs.avdeling%20=%20"UREAL")&page=' + page;
-		} else {
-			//url = "https://ask.bibsys.no/ask2/json/result.jsp?jsonp=?" + "&cql=" + term + '%20AND%20(bs.avdeling%20=%20"UREAL"%20OR(bs.bibkode%20=%20"k"%20AND%20bs.form%20=%20"n"))&page=' + page;
-			url = "https://ask.bibsys.no/ask2/json/result.jsp?" + window.JSONP + "&cql=%22" + term + '%22%20AND%20(bs.avdeling%20=%20"UREAL"%20OR(bs.bibkode%20=%20"k"%20AND%20bs.form%20=%20"n"))&page=' + page;
+
+		var cql, url;
+
+		if (term.substr(0,3) !== 'bs.') {
+			cql = '%22' + term + '%22';
+		} else { 
+			// The term is already valid CQL, for instance 'bs.objektid="...."', 'bs.isbn="...."'
+			// so we don't have to escape it
+			cql = term;
+			term = '';  // clear search field to avoid showing complex search query to the user
 		}
-		
+
+		if (window.BookWorms.includeEbooks == "off") {
+			cql += '%20AND%20(bs.avdeling%20=%20"UREAL")';
+		} else {
+			cql += '%20AND%20(bs.avdeling%20=%20"UREAL"%20OR(bs.bibkode%20=%20"k"%20AND%20bs.form%20=%20"n"))';
+		}
+		url = 'https://ask.bibsys.no/ask2/json/result.jsp?' + window.JSONP + '&cql=' + cql + '&page=' + page;
 		$.getJSON(url, function (data) {
 			BookWorms.showSearchResults(data,term,page,urlObj,options,decodeURIComponent(term));
-		});				
+		});
 	},
 
 	showSearchResults : function(data,term,pagenr,urlObj,options,query) {
@@ -435,15 +442,23 @@ $.extend(BookWorms,{
 	},
 
 	searchByRecordId: function(knyttdokid) {
-		var url = "http://labs.biblionaut.net/services/getids.php?" + window.JSONP + "&id=" + knyttdokid;
+		var url = "http://biblionaut.net/services/getids.php?" + window.JSONP + "&id=" + knyttdokid;
 		$.getJSON(url, function (data) {
-			$("#searchinput1").val('bs.objektid="' + data.objektid + '"');
-			$("#search_button").click();
+			if (data.objektid === '') {
+				alert("Sorry, this barcode could not be searched");
+			} else {
+				$("#searchinput2").val('bs.objektid="' + data.objektid + '"');
+				$("#home_search_button").click();
+			}
 		})
 		.error(function(){
 			$.mobile.hidePageLoadingMsg();
 			$('#block-ui').hide();
 		});
+	},
+	
+	unknownBarcodeFound: function(barcode) {
+		 $.mobile.changePage("#unknown_barcode", {transition: 'none', role: 'dialog'});
 	},
 
 	scanNow : function() {
@@ -455,7 +470,8 @@ $.extend(BookWorms,{
 					format = 'unknown';
 				// Only numerals?
 
-				if (barcode.match('[0-9X]+')[0] === barcode) {
+				var tmp = barcode.match('^[0-9X]{10,13}$');
+				if (tmp && tmp[0] === barcode) {
 					if (barcode.length === 10) {
 						format = 'isbn10';
 						if (!BookWorms.validIsbn10(barcode)) {
@@ -469,24 +485,22 @@ $.extend(BookWorms,{
 							return;
 						}
 					} 
-				} else {
-					if (barcode.length === 9) {
-						format = 'dokid'; // dokid eller knyttid
-					}
+				} else if (barcode.length === 9 && barcode.match('^[0-9]{2}')) {
+					format = 'dokid'; // dokid eller knyttid
 				}
 				if (format === 'isbn10' || format === 'isbn13') {
-					$("#searchinput1").val('bs.isbn="' + barcode + '"');
-					$("#search_button").click();
+					$("#searchinput2").val('bs.isbn="' + barcode + '"');
+					$("#home_search_button").click();
 				} else if (format === 'dokid') {
 					BookWorms.searchByRecordId(barcode);
 					return;
 				} else {
-					alert("Unknown barcode format found.");
+					BookWorms.unknownBarcodeFound(barcode);
 					return;
 				}
 			}, 
 			function(error) {
-			alert("Scanning failed: " + error);
+				alert("Scanning failed: " + error);
 			}
 		);
 	},		

Realfagsbibilioteket - UiO/assets/www/index.html
@@ -448,6 +448,22 @@
 		</div>		
 
 
+        <!-- Unknown barcode dialog -->
+		<div data-role="page" id="unknown_barcode">
+			<div data-role="header" data-theme="d">
+				<h1>Unknown barcode</h1>
+			</div>
+
+			<div data-role="content" data-theme="d">
+				<p>
+                    Sorry, the barcode you scanned is not supported.
+                </p>
+
+                <a href="javascript:;" data-role="button" data-rel="back" data-theme="b">Close</a>
+			</div>
+		</div>
+
+
         <!-- Location and hours -->
 		<div data-role="page" id="page_locationhours">
 			<div data-theme="a" data-role="header" data-position="fixed" data-id="foo">