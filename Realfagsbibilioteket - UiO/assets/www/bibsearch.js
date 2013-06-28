saqimtiaz/BibSearch

commit 744a27601316e0df5ed29d337533589ec44b5a8b

Fixed a bug introduced in 4f32c094716 concerning TAPT status

Realfagsbibilioteket - UiO/assets/www/bibsearch.js
@@ -39,10 +39,26 @@ $.extend(BookWorms,{
 			});
 
 			//XXX only showing first book [OLD]
-			//XXX Changed to prioritizing the first book with lending status avaiable. If no book found with lending status avaiable show first book found from UREAL
+			//Changed to prioritizing the first book with lending status avaiable. If no book found with lending status avaiable show first book found from UREAL
+			// Added check for TAPT
+			// If UREAL had an object, but it has been lost (status: 'tapt'), the Ask2 API still returns the object on a search limited to UREAL, 
+			// but the UREAL item(s) are not returned. Items from other libraries will still show up, 
+			// so if the list contains only items from other libraries (or no items at all), it means we 
+			// had the object at some time, but no more.
+			// We still show the item but show it as unavailable (we added TAPT as a condition for unavailable).
 
-			if (filtered[0]) var book = filtered[0];
-			else var book = firstbook;
+			var book;
+			if (filtered[0]) {
+				book = filtered[0];
+			}
+			else {
+				if (firstbook == "") {
+					book = {status: "TAPT"};
+				}
+				else {
+					book = firstbook;
+				}
+			}
 		
 			data.result.documents[0] = $.extend(data.result.documents[0], book);
 
@@ -141,10 +157,10 @@ $.extend(BookWorms,{
 		
 		$("#fav_star_button_link").attr("href","javascript:(function(){BookWorms.toggleFavorite('" + recordId + "');})()");
 		$("#favorite_book_button").attr("href","javascript:(function(){BookWorms.toggleFavorite('" + recordId + "');})()");
-		if (data.result.documents[0].lending_status == "UTL" || data.result.documents[0].lending_status == "UTL/RES") {
+		if ($.inArray(data.result.documents[0].lending_status, ["UTL", "UTL/RES", "TAPT"])) {
 			$("#status_indicators").addClass("book_unavailable").removeClass("book_available").removeClass("book_ordered");
 			$("#button_where_is_it").addClass("ui-disabled");
-		} else if (data.result.documents[0].status == "best" || data.result.documents[0].status == "akset") {
+		} else if ($.inArray(data.result.documents[0].status, ["best", "akset"])) {
 			$("#status_indicators").addClass("book_ordered").removeClass("book_available").removeClass("book_unavailable");
 			$("#button_where_is_it").addClass("ui-disabled");
 		} else {
@@ -266,16 +282,6 @@ $.extend(BookWorms,{
 		url = 'https://ask.bibsys.no/ask2/json/result.jsp?' + window.JSONP + '&cql=' + cql + '&page=' + page;
 		$.getJSON(url, function (data) {
 			
-			// If UREAL had an object, but it has been lost (status: 'tapt') or taken out of collection 
-			// (status: 'kass'), the Ask2 API still returns the object on a search limited to UREAL, 
-			// but the UREAL item(s) are not returned. Items from other libraries will still show up, 
-			// so if the list contains only items from other libraries (or no items at all), it means we 
-			// had the object at some time, but no more, and we should remove it from the result list.
-			data.result.documents = $.grep(data.result.documents, function(n, i) {
-				return (n.institutionsection == "UREAL") ? true : false;
-			});
-			data.result.totalHits = data.result.documents.length;
-
 			if (data.result.totalHits == 0)
 			{
 				BookWorms.showSearchResults(data,term,page,urlObj,options,decodeURIComponent(term));

commit 3b170a15e65255c481f76712622233558ff168ef

Bugfix II: status->lending_status for TAPT, wrong use of inArray, fix for series title lookup
1) status->lending_status for TAPT
2) wrong use of inArray introduced in 744a2760131
3) make sure result list is only updated after all series title checks
have completed

Realfagsbibilioteket - UiO/assets/www/bibsearch.js
@@ -53,7 +53,7 @@ $.extend(BookWorms,{
 			}
 			else {
 				if (firstbook == "") {
-					book = {status: "TAPT"};
+					book = {lending_status: "TAPT"};
 				}
 				else {
 					book = firstbook;
@@ -157,10 +157,10 @@ $.extend(BookWorms,{
 		
 		$("#fav_star_button_link").attr("href","javascript:(function(){BookWorms.toggleFavorite('" + recordId + "');})()");
 		$("#favorite_book_button").attr("href","javascript:(function(){BookWorms.toggleFavorite('" + recordId + "');})()");
-		if ($.inArray(data.result.documents[0].lending_status, ["UTL", "UTL/RES", "TAPT"])) {
+		if ($.inArray(data.result.documents[0].lending_status, ["UTL", "UTL/RES", "TAPT"]) !== -1) {
 			$("#status_indicators").addClass("book_unavailable").removeClass("book_available").removeClass("book_ordered");
 			$("#button_where_is_it").addClass("ui-disabled");
-		} else if ($.inArray(data.result.documents[0].status, ["best", "akset"])) {
+		} else if ($.inArray(data.result.documents[0].status, ["best", "akset"]) !== -1) {
 			$("#status_indicators").addClass("book_ordered").removeClass("book_available").removeClass("book_unavailable");
 			$("#button_where_is_it").addClass("ui-disabled");
 		} else {
@@ -376,7 +376,7 @@ $.extend(BookWorms,{
 		// are series item. For each series item, we need to make an ajax call,
 		// and we keep track of the remaining pending tasks with a variable
 		// When there are no more remaining tasks pending, we call onDone()
-		window.BookWorms.pendingTasks = 0;
+		window.BookWorms.pendingTasks = documents.length;
 		$.each(documents, function(index, val) {
 			window.BookWorms.checkitemForSeriesTitle(val, function() {
 				if (window.BookWorms.pendingTasks === 0) {
@@ -388,8 +388,6 @@ $.extend(BookWorms,{
 	},
 
 	checkitemForSeriesTitle: function(doc, onDone) {
-
-		window.BookWorms.pendingTasks += 1;
 
 		if (doc.series != '') {
 			// This object is a series item. We look up the series title